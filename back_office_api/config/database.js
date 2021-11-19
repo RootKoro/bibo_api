e8de8fa37898b37ba5b6d70ca4668f655d98f654
RootKoro <cyrmathieugueye@esp.sn>
2021-11-19 01:19:00 +0000

    doc

--- back_office_api/config/database.js
+++ back_office_api/config/database.js
@@ -52,7 +52,7 @@ module.exports = {
         connection: {
             host: Env.get('DB_HOST', 'localhost'),
             port: Env.get('DB_PORT', '3306'),
-            user: Env.get('DB_USER', 'koro'),
+            user: Env.get('DB_USER', 'root'),
             password: Env.get('DB_PASSWORD', ''),
             database: Env.get('DB_DATABASE', 'tests')
         }
